6d391a7a611ca66c08a0a78473e8a35fd0e6858f
paullaster <paullasterokoth98@gmail.com>
2023-03-05 09:30:17 +0300

    commit changes

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const userData = require('./MOCK_DATA.json');
 const app = express();
 
 const port = 4545;
+
+
 
 app.listen(port, () => {
     console.log('listening on port ' + port);

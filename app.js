7f34d138744f2a1851ca50a61c4168431adee3e6
paullaster <paullasterokoth98@gmail.com>
2023-03-05 11:10:23 +0300

    commit changes

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ const Mutation = new GraphQLObjectType({
                 first_name: {type: GraphQLString},
                 last_name: {type: GraphQLString},
                 email: {type: GraphQLString},
-                
+                gender: {type: GraphQLString},
+                ip_address:{type: GraphQLString}
             }
         }
     }
